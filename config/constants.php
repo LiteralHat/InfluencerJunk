7fcd36152d9a8c1d3a6a2c8f3c668e8b40f6b3f1
LiteralHat <literalhatreloaded@gmail.com>
2024-07-01 09:06:31 +1000

    moved constants and config, updated gitignore

---
+++ config/constants.php
@@ -0,0 +1,13 @@
+<?php 
+
+include 'config.php';
+
+define('URL_LAYOUT',  BASE_URL . 'views/layout');
+define('URL_STYLESHEET', BASE_URL . 'css/styles.css');
+
+define('ELEMENT_HEADER', URL_LAYOUT . 'header.php');
+define('ELEMENT_HEADER', URL_LAYOUT . 'footer.php');
+
+
+
+
